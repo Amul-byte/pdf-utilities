11ca800cfdf654ec7a3db1427b497d982313695b
pxgamerz345@gmail.com <pxgamerz345@gmail.com>
2025-01-03 22:54:39 -0600

    delete unwanted files

--- project2.py
+++
@@ -1,8 +0,0 @@
-import os
-entries = os.listdir('folder')
-print(f"Contents of the directory '{'folder'}':")
-for entry in entries:
-    print(entry)
-    if entry.endswith(".png"):
-        os.rename(f"folder/{entry}",f"abc.png")
-       
